d66b164a246c6a6e98caf65cdf0b75028ffb9318
Robby Ranshous <rranshous@gmail.com>
2020-05-02 20:35:54 -0400

    removing returnss allocation

--- contracts/TrustyPin.sol
+++ contracts/TrustyPin.sol
@@ -1,6 +1,7 @@
 pragma solidity >=0.5.6 <0.7.0;
 
 import "@openzeppelin/contracts/math/SafeMath.sol";
+//import "@openzeppelin/contracts/access/Ownable.sol"; // onlyOwner
 
 contract Constants {
   uint8 internal constant STATE_REQUESTED = 2;
@@ -8,9 +9,11 @@ contract Constants {
 
 contract TrustyPin is Constants {
 
+
   using SafeMath for uint;
 
   struct Pin {
+    string ipfsHash;
     uint chunksAllocated;
     address owner;
     uint8 state;
@@ -21,26 +24,46 @@ contract TrustyPin is Constants {
   string[] ipfsHashes;
 
   constructor() public {
-    chunksAvailable = 100;
+    super;
+    chunksAvailable = 0;
   }
 
   function addPin(string memory _ipfsHash, uint _chunksToAllocate) public {
-    Pin memory pin = Pin(_chunksToAllocate, msg.sender, STATE_REQUESTED);
+    require(_chunksToAllocate > 0, 'No chunks allocated');
+    Pin memory pin = Pin(_ipfsHash, _chunksToAllocate, msg.sender, STATE_REQUESTED);
     chunksAvailable = chunksAvailable.sub(_chunksToAllocate, "Not enough chunksAvailable");
     ipfsHashes.push(_ipfsHash);
     pinsByContentHash[_ipfsHash] = pin;
   }
 
+  function removePin(string memory _ipfsHash) public {
+    Pin storage pin = pinsByContentHash[_ipfsHash];
+    require(pin.chunksAllocated > 0, 'Pin not found');
+    chunksAvailable = chunksAvailable.add(pin.chunksAllocated);
+  }
+
   function getPin(string memory _ipfsHash)
   public view returns (
-    uint chunksAllocated, address owner, uint8 state
+    string memory ipfsHash, uint chunksAllocated, address owner, uint8 state
   ) {
+    require(pinsByContentHash[_ipfsHash].chunksAllocated > 0, 'Pin not found');
     Pin storage pin = pinsByContentHash[_ipfsHash];
-    return (pin.chunksAllocated, pin.owner, pin.state);
+    return (pin.ipfsHash, pin.chunksAllocated, pin.owner, pin.state);
   }
 
   function getIpfsHashByIndex(uint _index) public view returns (string memory) {
+    require(ipfsHashes.length > _index, 'Index too large');
+    require(_index >= 0, 'Index below 0');
     return ipfsHashes[_index];
+  }
+
+  function getNumberOfPins() public view returns (uint) {
+    return ipfsHashes.length;
+  }
+
+  function setChunksAvailable(uint _chunksAvailable) public {
+    require(_chunksAvailable > 0, 'chunksAvailable must be more than 0');
+    chunksAvailable = _chunksAvailable;
   }
 }
 
--- test/trustypin.js
+++ test/trustypin.js
@@ -7,61 +7,87 @@ const states = {
 
 contract("TrustyPin", accounts => {
   let ipfsHash = "testHash",
+    ipfsHash2 = "testHash2",
+    initialChunksAvailable = 100,
     chunksToAllocate = 32,
-    initialRemainingChunks;
+    contractChunksAvailable,
+    trustyPinInstance;
+
+  beforeEach(async () => {
+    trustyPinInstance = await TrustyPin.deployed();
+    contractChunksAvailable = await trustyPinInstance.chunksAvailable();
+  });
+
+  it("can update chunksAvailable", async () => {
+    let chunksAvailable = await trustyPinInstance.chunksAvailable();
+    assert.equal(chunksAvailable, 0); // default
+    await trustyPinInstance.setChunksAvailable(initialChunksAvailable, { from: accounts[0] });
+    assert.equal(await trustyPinInstance.chunksAvailable(), initialChunksAvailable);
+  });
 
   it("can add and retrieve pins", async () => {
-    const trustyPinInstance = await TrustyPin.deployed();
-    initialRemainingChunks = await trustyPinInstance.chunksAvailable();
-
     await trustyPinInstance.addPin(ipfsHash, chunksToAllocate, { from: accounts[0] });
-
-    const pinDetails = await trustyPinInstance.getPin.call(ipfsHash);
-
+    const pinDetails = await trustyPinInstance.getPin(ipfsHash);
     assert.equal(pinDetails.chunksAllocated, 32);
   });
 
   it("defaults new pin's state to requested", async () => {
-    const trustyPinInstance = await TrustyPin.deployed();
-    const pinDetails = await trustyPinInstance.getPin.call(ipfsHash);
-
+    const pinDetails = await trustyPinInstance.getPin(ipfsHash);
     assert.equal(pinDetails.state, states.requested);
   });
 
   it("associates sender to pin as owner", async () => {
-    const trustyPinInstance = await TrustyPin.deployed();
-    const pinDetails = await trustyPinInstance.getPin.call(ipfsHash);
-
+    const pinDetails = await trustyPinInstance.getPin(ipfsHash);
     assert.equal(pinDetails.owner, accounts[0]);
   });
 
-  it("reduces available chunks with each added pin", async () => {
-    const trustyPinInstance = await TrustyPin.deployed();
-    const remainingChunks = await trustyPinInstance.chunksAvailable();
-    let expectedChunks = initialRemainingChunks - chunksToAllocate;
+  it("sets chunksAllocated on pin", async () => {
+    const pinDetails = await trustyPinInstance.getPin(ipfsHash);
+    assert.equal(pinDetails.chunksAllocated, chunksToAllocate);
+  });
 
-    assert.equal(remainingChunks, expectedChunks);
+  it("reduces available chunks with each added pin", async () => {
+    const remainingChunks = await trustyPinInstance.chunksAvailable();
+    assert.equal(remainingChunks, initialChunksAvailable - chunksToAllocate);
   });
 
   it("rejects transactions which would dip chunksAvailable below 0", async () => {
-    const trustyPinInstance = await TrustyPin.deployed();
-    initialRemainingChunks = await trustyPinInstance.chunksAvailable();
-
     await truffleAssert.reverts(
       trustyPinInstance.addPin(ipfsHash, 100, { from: accounts[0] }),
       "Not enough chunksAvailable"
     );
   });
 
-  it("enumerates ipfsHashes", async () => {
-    const trustyPinInstance = await TrustyPin.deployed();
-    let ipfsHash2 = "testHash2";
+  it("can track more than one pin", async () => {
     await trustyPinInstance.addPin(ipfsHash2, chunksToAllocate, { from: accounts[0] });
-    let expectedIpfsHashes = [ipfsHash, ipfsHash2];
+    let numberOfPins = await trustyPinInstance.getNumberOfPins();
+    assert.equal(numberOfPins.toNumber(), 2);
+    let pin1 = await trustyPinInstance.getPin(ipfsHash);
+    let pin2 = await trustyPinInstance.getPin(ipfsHash2);
+    assert.equal(pin1.ipfsHash, ipfsHash);
+    assert.equal(pin1.chunksAllocated, chunksToAllocate);
+    assert.equal(pin2.ipfsHash, ipfsHash2);
+    assert.equal(pin2.chunksAllocated, chunksToAllocate);
+  });
 
-    for(let i=0; i<2; i++) {
+  it("enumerates ipfsHashes", async () => {
+    let expectedIpfsHashes = [ipfsHash, ipfsHash2];
+    let numberOfPins = await trustyPinInstance.getNumberOfPins();
+    for(let i=0; i<numberOfPins; i++) {
       let ipfsHash = await trustyPinInstance.getIpfsHashByIndex(i);
       assert.equal(ipfsHash, expectedIpfsHashes[i]);
     }
+  });
+
+  it("allows owner to remove pin, returning allocated chunks", async () => {
+    let chunksAvailable = await trustyPinInstance.chunksAvailable();
+    let pin = await trustyPinInstance.getPin(ipfsHash2);
+    let pinnedChunks = pin.chunksAllocated;
+    assert.equal(chunksAvailable.toNumber(), initialChunksAvailable - chunksToAllocate * 2);
+    await  trustyPinInstance.removePin(ipfsHash2, { from: accounts[0] })
+    chunksAvailable = await trustyPinInstance.chunksAvailable();
+    assert.equal(chunksAvailable.toNumber(), contractChunksAvailable.toNumber() + chunksToAllocate);
+    await trustyPinInstance.getIpfsHashByIndex(1);
+    await trustyPinInstance.getPin(ipfsHash);
   });
 });
